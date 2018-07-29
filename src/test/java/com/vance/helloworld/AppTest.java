cbc34577c721b019d303b70e6003d402eb4ea8b4
fufengchang <3022931383@qq.com>
2018-07-29 19:16:22 +0800

    new update

--- src/test/java/com/vance/helloworld/AppTest.java
+++ src/test/java/com/vance/helloworld/AppTest.java
@@ -1,20 +1,31 @@
 package com.vance.helloworld;
 
-import static org.junit.Assert.assertTrue;
-
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    private OutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void before() {
+        System.setOut(new PrintStream(out));
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void printApp() {
+        App app = new App();
+        app.printApp();
+        String outStr = out.toString();
+//        Assert.assertEquals("APP......".equals(out.toString()), true);
+        Assert.assertThat(out.toString(), is("APP......"));
     }
 }
